63cdadf2ca9ca273e6b8570754e7e695f03437ba
Mirabela Rusu <mirabela.rusu@stanford.edu>
2018-08-15 18:03:56 -0700

    Added elastix/slicerElastix paths, status bar that doens't really work, masks that have not really been tested (but should work), added output on the transform (not fully tested)

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Slicer-RadPathFusion
 Contains the Slicer Extension for radiology pathology fusion
+
+# Install
+
+## Requirements
+
+* Download [3D Slicer](https://download.slicer.org/)
+* Download [Elastix 4.9.0](https://github.com/SuperElastix/elastix/releases/tag/4.9.0).
+* Clone    [SlicerElastix](https://github.com/lassoan/SlicerElastix)
+* Clone    Slicer-RadPathFusion (this repository) 
+
+## How to install 
+
+1. Open 3D Slicer
+2. Edit > Application Settings > Modules > Add
+3. Choose path to Slicer-RadPathFusion / RadPathFusion
+4. Choose Ok, Yes (Note! This will restart slicer)
+
+# How to use
+
+1. Open Slicer
+2. Modules > Radiology-Pathology Fusion
+
+
+# To do
+
+1. Multi-threaded
+2. Progress bar
+3. Dynamic instead of Hardcoded path
+
+ 
--- RadPathFusion/RadPathFusion.py
+++ RadPathFusion/RadPathFusion.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from __main__ import vtk, qt, ctk, slicer
 #
 # RadPathFusion
@@ -26,110 +28,249 @@ class RadPathFusion:
 #
 
 class RadPathFusionWidget:
-  def __init__(self, parent = None): #constructor 
-    #self.logic = RadPathFusionLogic()
-    if not parent:
-      self.parent = slicer.qMRMLWidget()
-      self.parent.setLayout(qt.QVBoxLayout())
-      self.parent.setMRMLScene(slicer.mrmlScene)
-    else:
-      self.parent = parent
-    self.layout = self.parent.layout()
-    if not parent:
-      self.setup()
-      self.parent.show()
-    self.logic = None
-    self.slicerElastixPath = None
+    def __init__(self, parent = None): #constructor 
+        #self.logic = RadPathFusionLogic()
+        if not parent:
+          self.parent = slicer.qMRMLWidget()
+          self.parent.setLayout(qt.QVBoxLayout())
+          self.parent.setMRMLScene(slicer.mrmlScene)
+        else:
+          self.parent = parent
+        self.layout = self.parent.layout()
+        if not parent:
+          self.setup()
+          self.parent.show()
+        self.logic = RadPathFusionLogic()
+        self.slicerElastixPath = None
+        self.verbose = True
 
-  def setup(self):
-    # Collapsible button
-    self.inputCollapsibleButton = ctk.ctkCollapsibleButton()
-    self.inputCollapsibleButton.text = "Inputs"
-    self.layout.addWidget(self.inputCollapsibleButton)
+    def setup(self):
+        #
+        # Configuration
+        #
+        self.configCollapsibleButton = ctk.ctkCollapsibleButton()
+        self.configCollapsibleButton.text = "Configuration"
+        self.layout.addWidget(self.configCollapsibleButton)
 
-    # Layout within the input collapsible button
-    self.inputFormLayout = qt.QFormLayout(self.inputCollapsibleButton)
+        # Layout within the configuration collapsible button
+        self.configFormLayout = qt.QFormLayout(self.configCollapsibleButton)
+     
 
-    #
-    # the volume selectors
-    #
-    self.inputFixedFrame = qt.QFrame(self.inputCollapsibleButton)
-    self.inputFixedFrame.setLayout(qt.QHBoxLayout())
-    self.inputFormLayout.addWidget(self.inputFixedFrame)
-    self.inputFixedSelector = qt.QLabel("Fixed Volume: ", self.inputFixedFrame)
-    self.inputFixedFrame.layout().addWidget(self.inputFixedSelector)
-    self.inputFixedSelector = slicer.qMRMLNodeComboBox(self.inputFixedFrame)
-    self.inputFixedSelector.nodeTypes = ( ("vtkMRMLScalarVolumeNode"), "" )
-    self.inputFixedSelector.addEnabled = False
-    self.inputFixedSelector.removeEnabled = False
-    self.inputFixedSelector.setMRMLScene( slicer.mrmlScene )
-    self.inputFixedFrame.layout().addWidget(self.inputFixedSelector)
+        import platform
+        self.elastixPath = ctk.ctkPathLineEdit()
+        self.elastixPath.filters = ctk.ctkPathLineEdit.Dirs
+        self.configFormLayout.addRow("Elastix Executable Path:", self.elastixPath)
+        
+        if platform.system() == 'Linux':
+            self.elastixPath.setCurrentPath('/home/mrusu/Programs/elastix-4.9.0-linux/bin/')
+        if platform.system() == 'Windows':
+            self.elastixPath.setCurrentPath("C:/Programs/elastix-4.9.0-win64/")
 
-    self.inputMovingFrame = qt.QFrame(self.inputCollapsibleButton)
-    self.inputMovingFrame.setLayout(qt.QHBoxLayout())
-    self.inputFormLayout.addWidget(self.inputMovingFrame)
-    self.inputMovingSelector = qt.QLabel("Moving Volume: ", self.inputMovingFrame)
-    self.inputMovingFrame.layout().addWidget(self.inputMovingSelector)
-    self.inputMovingSelector = slicer.qMRMLNodeComboBox(self.inputMovingFrame)
-    self.inputMovingSelector.nodeTypes = ( ("vtkMRMLScalarVolumeNode"), "" )
-    self.inputMovingSelector.addEnabled = False
-    self.inputMovingSelector.removeEnabled = False
-    self.inputMovingSelector.setMRMLScene( slicer.mrmlScene )
-    self.inputMovingFrame.layout().addWidget(self.inputMovingSelector)
+        self.slicerElastixPath = ctk.ctkPathLineEdit()
+        self.slicerElastixPath.filters = ctk.ctkPathLineEdit.Dirs
+        self.configFormLayout.addRow("Slicer Elastix Path:", self.slicerElastixPath)
 
+        if platform.system() == 'Linux':
+            self.slicerElastixPath.setCurrentPath('/home/mrusu/Programs/SlicerElastix/Elastix')
+        if platform.system() == 'Windows':
+            self.slicerElastixPath.setCurrentPath("C:/Programs/SlicerElastix/Elastix/")
 
-    self.outputCollapsibleButton = ctk.ctkCollapsibleButton()
-    self.outputCollapsibleButton.text = "Output"
-    self.layout.addWidget(self.outputCollapsibleButton)
+     
+        #
+        # Input 
+        #
+        self.inputCollapsibleButton = ctk.ctkCollapsibleButton()
+        self.inputCollapsibleButton.text = "Input"
+        self.layout.addWidget(self.inputCollapsibleButton)
 
-    # Layout within the output collapsible button
-    self.outputFormLayout = qt.QFormLayout(self.outputCollapsibleButton)
+        # Layout within the input collapsible button
+        self.inputFormLayout = qt.QFormLayout(self.inputCollapsibleButton)
 
+        #
+        # fixed volume selector
+        #
+        self.fixedVolumeSelector = slicer.qMRMLNodeComboBox()
+        self.fixedVolumeSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+        self.fixedVolumeSelector.selectNodeUponCreation = True
+        self.fixedVolumeSelector.addEnabled = False
+        self.fixedVolumeSelector.removeEnabled = False
+        self.fixedVolumeSelector.noneEnabled = False
+        self.fixedVolumeSelector.showHidden = False
+        self.fixedVolumeSelector.showChildNodeTypes = False
+        self.fixedVolumeSelector.setMRMLScene( slicer.mrmlScene )
+        self.inputFormLayout.addRow("Fixed volume: ", self.fixedVolumeSelector)
 
-    self.outputFrame = qt.QFrame(self.outputCollapsibleButton)
-    self.outputFrame.setLayout(qt.QHBoxLayout())
-    self.outputFormLayout.addWidget(self.outputFrame)
-    self.outputSelector = qt.QLabel("Output Volume: ", self.outputFrame)
-    self.outputFrame.layout().addWidget(self.outputSelector)
-    self.outputSelector = slicer.qMRMLNodeComboBox(self.outputFrame)
-    self.outputSelector.nodeTypes = ( ("vtkMRMLScalarVolumeNode"), "" )
-    self.outputSelector.setMRMLScene( slicer.mrmlScene )
-    self.outputFrame.layout().addWidget(self.outputSelector)
+        
+        self.fixedVolumeMaskSelector = slicer.qMRMLNodeComboBox()
+        self.fixedVolumeMaskSelector.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+        self.fixedVolumeMaskSelector.addEnabled = False
+        self.fixedVolumeMaskSelector.removeEnabled = False
+        self.fixedVolumeMaskSelector.noneEnabled = True
+        self.fixedVolumeMaskSelector.showHidden = False
+        self.fixedVolumeMaskSelector.showChildNodeTypes = False
+        self.fixedVolumeMaskSelector.setMRMLScene( slicer.mrmlScene )
+        self.inputFormLayout.addRow("Fixed volume mask: ", self.fixedVolumeMaskSelector)
 
-
-    # Apply button
-    self.applyButton = qt.QPushButton("Apply")
-    self.applyButton.toolTip = "Run the Radiology-Pathology Fusion."
-    self.layout.addWidget(self.applyButton)
-
-    self.applyButton.connect('clicked(bool)', self.onApply)
-
-    # Add vertical spacer
-    self.layout.addStretch(1)
-
-  def onApply(self):
-    print('onApply')
-    #FIXME: make logic globab
-    self.logic = RadPathFusionLogic()
-    #self.logic.testElastixLogic()
-    self.slicerElastixPath = "/home/mrusu/.config/NA-MIC/Extensions-27350/SlicerElastix/lib/Slicer-4.9/Resources"
-    self.logic.SetSlicerElastixPath(self.slicerElastixPath)
-
-    self.logic.run(self.inputFixedSelector.currentNode(), 
-        self.inputMovingSelector.currentNode(),
-        outputVolumeNode = self.outputSelector.currentNode(),
-        #outputTransformNode = self.outputTransformSelector.currentNode(),
-        #fixedVolumeMaskNode = self.fixedVolumeMaskSelector.currentNode(),
-        #movingVolumeMaskNode = self.movingVolumeMaskSelector.currentNode()
-        )
+        self.movingVolumeSelector = slicer.qMRMLNodeComboBox()
+        self.movingVolumeSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+        self.movingVolumeSelector.selectNodeUponCreation = True
+        self.movingVolumeSelector.addEnabled = False
+        self.movingVolumeSelector.removeEnabled = False
+        self.movingVolumeSelector.noneEnabled = False
+        self.movingVolumeSelector.showHidden = False
+        self.movingVolumeSelector.showChildNodeTypes = False
+        self.movingVolumeSelector.setMRMLScene( slicer.mrmlScene )
+        self.inputFormLayout.addRow("Moving volume: ", self.movingVolumeSelector)
 
 
-        #print('onApply')
-        #self.logic = DeepInferLogic()
-        # try:
-        #self.currentStatusLabel.text = "Starting"
-        #self.modelParameters.prerun()
-        #self.logic.run(self.modelParameters)
+        #
+        # moving volume mask selector
+        #
+        self.movingVolumeMaskSelector = slicer.qMRMLNodeComboBox()
+        self.movingVolumeMaskSelector.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+        self.movingVolumeMaskSelector.selectNodeUponCreation = True
+        self.movingVolumeMaskSelector.addEnabled = False
+        self.movingVolumeMaskSelector.removeEnabled = False
+        self.movingVolumeMaskSelector.noneEnabled = True
+        self.movingVolumeMaskSelector.showHidden = False
+        self.movingVolumeMaskSelector.showChildNodeTypes = False
+        self.movingVolumeMaskSelector.setMRMLScene( slicer.mrmlScene )
+        self.movingVolumeMaskSelector.setToolTip("Moving volume mask")
+        self.inputFormLayout.addRow("Moving volume mask: ", self.movingVolumeMaskSelector)
+
+
+        #
+        #Output
+        #
+        self.outputCollapsibleButton = ctk.ctkCollapsibleButton()
+        self.outputCollapsibleButton.text = "Output"
+        self.layout.addWidget(self.outputCollapsibleButton)
+
+        # Layout within the output collapsible button
+        self.outputFormLayout = qt.QFormLayout(self.outputCollapsibleButton)
+
+        #
+        # output volume selector
+        #
+        self.outputVolumeSelector = slicer.qMRMLNodeComboBox()
+        self.outputVolumeSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+        self.outputVolumeSelector.selectNodeUponCreation = True
+        self.outputVolumeSelector.addEnabled = True
+        self.outputVolumeSelector.renameEnabled = True
+        self.outputVolumeSelector.removeEnabled = True
+        self.outputVolumeSelector.noneEnabled = True
+        self.outputVolumeSelector.showHidden = False
+        self.outputVolumeSelector.showChildNodeTypes = False
+        self.outputVolumeSelector.setMRMLScene( slicer.mrmlScene )
+        self.outputFormLayout.addRow("Output volume: ", self.outputVolumeSelector)
+
+        #
+        # output transform selector
+        #
+        self.outputTransformSelector = slicer.qMRMLNodeComboBox()
+        self.outputTransformSelector.nodeTypes = ["vtkMRMLTransformNode"]
+        self.outputTransformSelector.selectNodeUponCreation = True
+        self.outputTransformSelector.addEnabled = True
+        self.outputTransformSelector.renameEnabled = True
+        self.outputTransformSelector.removeEnabled = True
+        self.outputTransformSelector.noneEnabled = True
+        self.outputTransformSelector.showHidden = False
+        self.outputTransformSelector.showChildNodeTypes = False
+        self.outputTransformSelector.setMRMLScene( slicer.mrmlScene )
+        self.outputFormLayout.addRow("Output transform: ", self.outputTransformSelector)
+
+       # Add vertical spacer
+        self.layout.addStretch(1)
+
+
+        #
+        # Status and Progress
+        #
+        statusLabel = qt.QLabel("Status: ")
+        self.currentStatusLabel = qt.QLabel("Idle")
+        hlayout = qt.QHBoxLayout()
+        hlayout.addStretch(1)
+        hlayout.addWidget(statusLabel)
+        hlayout.addWidget(self.currentStatusLabel)
+        self.layout.addLayout(hlayout)
+
+        self.progress = qt.QProgressBar()
+        self.progress.setRange(0, 1000)
+        self.progress.setValue(0)
+        self.layout.addWidget(self.progress)
+        self.progress.hide()
+
+        #Cancel
+        self.cancelButton = qt.QPushButton("Cancel")
+        self.cancelButton.toolTip = "Abort the algorithm."
+        self.cancelButton.enabled = False
+
+        # Apply button
+        self.applyButton = qt.QPushButton("Apply")
+        self.applyButton.toolTip = "Run the Radiology-Pathology Fusion."
+        self.applyButton.enabled = True
+
+        # Test button
+        self.testButton = qt.QPushButton("Test")
+        self.testButton.toolTip = "Text elastix."
+        self.testButton.enabled = True
+
+        hlayout = qt.QHBoxLayout()
+
+        hlayout.addWidget(self.testButton)
+        hlayout.addStretch(1)
+        hlayout.addWidget(self.cancelButton)
+        hlayout.addWidget(self.applyButton)
+        self.layout.addLayout(hlayout)
+
+
+        self.cancelButton.connect('clicked(bool)', self.onCancel)
+        self.applyButton.connect('clicked(bool)', self.onApply)
+        self.testButton.connect('clicked(bool)', self.onTest)
+
+    def onApply(self):
+        if self.verbose:
+            print('onApply')
+
+        self.logic.SetSlicerElastixPath(self.slicerElastixPath.currentPath)
+        self.logic.SetElastixPath(self.elastixPath.currentPath)
+
+        self.logic.run(self.fixedVolumeSelector.currentNode(), 
+            self.movingVolumeSelector.currentNode(),
+            outputVolumeNode = self.outputVolumeSelector.currentNode(),
+            outputTransformNode = self.outputTransformSelector.currentNode(),
+            fixedVolumeMaskNode = self.fixedVolumeMaskSelector.currentNode(),
+            movingVolumeMaskNode = self.movingVolumeMaskSelector.currentNode()
+            )
+    def onCancel(self):
+        if self.verbose:
+            print('onCancel')
+
+    def onTest(self):
+        if self.verbose:
+            print('onTest')
+
+        self.logic.SetSlicerElastixPath(self.slicerElastixPath.currentPath)
+        self.logic.SetElastixPath(self.elastixPath.currentPath)
+
+        self.logic.testElastixLogic()
+
+    def onLogicEventStart(self):
+        self.currentStatusLabel.text = "Running"
+        self.cancelButton.setDisabled(False)
+        self.progress.setValue(0)
+        self.progress.show()
+
+    def onLogicEventEnd(self):
+        self.currentStatusLabel.text = "Completed"
+        self.progress.setValue(1000)
+
+    def onLogicEventAbort(self):
+        self.currentStatusLabel.text = "Aborted"
+
+    def onLogicEventProgress(self, progress):
+        self.currentStatusLabel.text = "Running ({0:6.5f})".format(progress)
+        self.progress.setValue(progress * 1000)
 
 
 #
@@ -139,18 +280,42 @@ class RadPathFusionLogic():
     def __init__(self):
         import os
         self.slicerElastixPath = None
+        self.elastixPath = None
         self.scriptPath = os.path.dirname(os.path.abspath(__file__))
         self.registrationParameterFilesDir = os.path.abspath(os.path.join(self.scriptPath, 
             'Resources', 
             'RegistrationParameters'))
 
-    def run(self, fixedVolume, movingVolume, outputVolumeNode = None):
+        self.verbose = True
+
+    def run(self, fixedVolume, movingVolume, 
+        outputVolumeNode     = None,
+        outputTransformNode  = None,
+        fixedVolumeMaskNode  = None,
+        movingVolumeMaskNode = None):
+
+        modules = slicer.modules
+        if hasattr(modules, 'RadPathFusionWidget'):
+            widgetPresent = True
+        else:
+            widgetPresent = False
+
+        if widgetPresent:
+            self.cmdStartEvent()
+    
+        if self.verbose:
+            print("SlicerElastixPath:",self.slicerElastixPath)
+            print("ElastixPath:",self.elastixPath)
+    
         import sys
         if self.slicerElastixPath:
             sys.path.append(self.slicerElastixPath)
         else:
             print("Please set the path to Elastics")
             return 0
+
+        if self.verbose:
+            print("path append was succesful", self.slicerElastixPath)
 
         try:
             from Elastix import ElastixLogic
@@ -161,17 +326,22 @@ class RadPathFusionLogic():
        
         logic = ElastixLogic()
         parameterFilenames = ["QuickCenteringRigid.txt", 
-            "Similarity.txt", 
-            "Affine.txt", 
-            "Deformable.txt"]
+            "Similarity.txt"]#, 
+            #"Affine.txt", 
+            #"Deformable.txt"]
         logic.registrationParameterFilesDir =  self.registrationParameterFilesDir
-        logic.registerVolumes(fixedVolume, movingVolume, 
-            parameterFilenames = parameterFilenames, 
-            outputVolumeNode = outputVolumeNode)
+        logic.setCustomElastixBinDir(self.elastixPath)
 
-        selectionNode = slicer.app.applicationLogic().GetSelectionNode()
-        selectionNode.SetReferenceActiveVolumeID(outputVolumeNode.GetID())
-        slicer.app.applicationLogic().PropagateVolumeSelection(0)
+        logic.registerVolumes(fixedVolume, movingVolume, 
+            parameterFilenames   = parameterFilenames, 
+            outputVolumeNode     = outputVolumeNode,
+            outputTransformNode  = outputTransformNode,
+            fixedVolumeMaskNode  = fixedVolumeMaskNode,
+            movingVolumeMaskNode = movingVolumeMaskNode)
+
+        if widgetPresent:
+            self.cmdEndEvent()
+
 
     def testElastixLogic(self):
         print("Starting the test")
@@ -218,3 +388,40 @@ class RadPathFusionLogic():
 
     def SetSlicerElastixPath(self, path):
         self.slicerElastixPath = path
+
+    def SetElastixPath(self, path):
+        self.elastixPath = path
+
+    def yieldPythonGIL(self, seconds=0):
+        sleep(seconds)
+
+    def cmdCheckAbort(self, p):
+        if self.abort:
+            p.kill()
+            self.cmdAbortEvent()
+
+    def cmdStartEvent(self):
+        if hasattr(slicer.modules, 'RadPathFusionWidget'):
+            widget = slicer.modules.RadPathFusionWidget
+            widget.onLogicEventStart()
+        self.yieldPythonGIL()
+
+    def cmdProgressEvent(self, progress):
+        if hasattr(slicer.modules, 'RadPathFusionWidget'):
+            widget = slicer.modules.RadPathFusionWidget
+            widget.onLogicEventProgress(progress)
+        self.yieldPythonGIL()
+
+    def cmdAbortEvent(self):
+        if hasattr(slicer.modules, 'RadPathFusionWidget'):
+            widget = slicer.modules.RadPathFusionWidget
+            widget.onLogicEventAbort()
+        self.yieldPythonGIL()
+
+    def cmdEndEvent(self):
+        if hasattr(slicer.modules, 'RadPathFusionWidget'):
+            widget = slicer.modules.RadPathFusionWidget
+            widget.onLogicEventEnd()
+        self.yieldPythonGIL()
+
+
