e54a12dc628606ac35da51f1932f184c8c5be46c
Mirabela Rusu <mirabela.rusu@gmail.com>
2019-04-26 13:33:33 -0700

    Added functionality to run the registration with in vivo images

--- RadPathFusion/ParsePathJson.py
+++ RadPathFusion/ParsePathJson.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import json
+from time import sleep
 from __main__ import vtk, qt, ctk, slicer
 #
 # ParsePathJson
@@ -63,12 +64,42 @@ class ParsePathJsonWidget:
         self.inputFormLayout.addRow("Input Json:", self.inputJsonFn)
         #self.inputJsonFn.setCurrentPath('input.json')
         if platform.system() == 'Linux':
-            self.inputJsonFn.setCurrentPath('/home/mrusu/Projects/RadPathFusion/prostate/4_histology/3_1613543.json')
+            self.inputJsonFn.setCurrentPath('/home/mrusu/Projects/RadPathFusion/prostate/4_histology/')
         if platform.system() == 'Windows':
-            self.inputJsonFn.setCurrentPath('C:/Projects/rad-path-fusion/prostate_RP/3_histology/RP-66-1730563.json')
+            self.inputJsonFn.setCurrentPath('C:/Projects/rad-path-fusion/prostate_RP/3_histology/')
         #self.inputJsonFn.setMaximumWidth(425)
- 
+        self.inputJsonFn.addCurrentPathToHistory();
 
+        #
+        # input fixed volume selector
+        #
+        self.inputVolumeSelector = slicer.qMRMLNodeComboBox()
+        self.inputVolumeSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+        self.inputVolumeSelector.selectNodeUponCreation = True
+        self.inputVolumeSelector.addEnabled = False
+        self.inputVolumeSelector.renameEnabled = True
+        self.inputVolumeSelector.removeEnabled = True
+        self.inputVolumeSelector.noneEnabled = True
+        self.inputVolumeSelector.showHidden = False
+        self.inputVolumeSelector.showChildNodeTypes = False
+        self.inputVolumeSelector.setMRMLScene( slicer.mrmlScene )
+        self.inputFormLayout.addRow("Fixed Volume: ", self.inputVolumeSelector)
+
+        #
+        # input fixed volume selector
+        #
+        self.inputMaskSelector = slicer.qMRMLNodeComboBox()
+        self.inputMaskSelector.nodeTypes = ["vtkMRMLLabelMapVolumeNode"]
+        self.inputMaskSelector.selectNodeUponCreation = True
+        self.inputMaskSelector.addEnabled = False
+        self.inputMaskSelector.renameEnabled = True
+        self.inputMaskSelector.removeEnabled = True
+        self.inputMaskSelector.noneEnabled = True
+        self.inputMaskSelector.showHidden = False
+        self.inputMaskSelector.showChildNodeTypes = False
+        self.inputMaskSelector.setMRMLScene( slicer.mrmlScene )
+        self.inputFormLayout.addRow("Fixed Mask: ", self.inputMaskSelector)
+        
         #
         #Output
         #
@@ -83,9 +114,9 @@ class ParsePathJsonWidget:
         self.outputJsonFn = ctk.ctkPathLineEdit()
         self.outputFormLayout.addRow("Output Json:", self.outputJsonFn)
         if platform.system() == 'Linux':
-            self.outputJsonFn.setCurrentPath('/home/mrusu/Projects/RadPathFusion/prostate/4_histology/3_1613543_test.json')
+            self.outputJsonFn.setCurrentPath('/home/mrusu/Projects/RadPathFusion/prostate/4_histology/')
         if platform.system() == 'Windows':
-            self.outputJsonFn.setCurrentPath( 'C:/Projects/rad-path-fusion/prostate_RP/3_histology/RP-66-1730563_test.json')
+            self.outputJsonFn.setCurrentPath( 'C:/Projects/rad-path-fusion/prostate_RP/3_histology/')
             
         #self.outputJsonFn.setMaximumWidth(400)
         """
@@ -128,20 +159,7 @@ class ParsePathJsonWidget:
         self.outputFormLayout.addRow("Mask ID", self.maskIdSelector)
         #self.outputMaskVolumeSelector.setMaximumWidth(400)
 
-        #
-        # advanced 
-        #
-        self.advancedCollapsibleButton = ctk.ctkCollapsibleButton()
-        self.advancedCollapsibleButton.text = "Advanced"
-        self.layout.addWidget(self.advancedCollapsibleButton)
-        self.advancedCollapsibleButton.enabled = False
-        
-        self.advancedFormLayout = qt.QFormLayout(self.advancedCollapsibleButton)
 
-        
-
-        # Add vertical spacer
-        self.layout.addStretch(1)
 
 
         #
@@ -156,8 +174,8 @@ class ParsePathJsonWidget:
         self.layout.addLayout(hlayout)
 
         self.progress = qt.QProgressBar()
-        self.progress.setRange(0, 1000)
-        self.progress.setValue(0)
+        self.progress.setRange(0, 0)
+        #self.progress.setValue(0)
         self.layout.addWidget(self.progress)
         self.progress.hide()
 
@@ -171,6 +189,16 @@ class ParsePathJsonWidget:
         self.saveJsonButton.toolTip = "Save the json file."
         self.saveJsonButton.enabled = False
 
+        # ReconstructVolume
+        self.refineVolumeButton = qt.QPushButton("Refine Volume")
+        self.refineVolumeButton.toolTip = "Refine."
+        self.refineVolumeButton.enabled = True
+
+        # RegisterVolume
+        self.registerVolumeButton = qt.QPushButton("Register")
+        self.registerVolumeButton.toolTip = "Register"
+        self.registerVolumeButton.enabled = True
+        
         # LoadVolume
         self.loadVolumeButton = qt.QPushButton("Load Volume")
         self.loadVolumeButton.toolTip = "Load Volume."
@@ -181,12 +209,19 @@ class ParsePathJsonWidget:
         self.loadMaskVolumeButton.toolTip = "Load a Mask"
         self.loadMaskVolumeButton.enabled = True
 
+        # Clear
+        self.clearAllVolumeButton = qt.QPushButton("Clear ALL")
+        self.clearAllVolumeButton.toolTip = "Clear"
+        self.clearAllVolumeButton.enabled = True
 
         hlayout = qt.QHBoxLayout()
 
         hlayout.addWidget(self.loadJsonButton)
+        hlayout.addWidget(self.refineVolumeButton)
+        hlayout.addWidget(self.registerVolumeButton)
         hlayout.addWidget(self.loadVolumeButton)
         hlayout.addWidget(self.loadMaskVolumeButton)
+        hlayout.addWidget(self.clearAllVolumeButton)
         hlayout.addStretch(1)
         hlayout.addWidget(self.saveJsonButton)
         self.layout.addLayout(hlayout)
@@ -195,12 +230,31 @@ class ParsePathJsonWidget:
         self.loadJsonButton.connect('clicked(bool)', self.onLoadJson)
         self.saveJsonButton.connect('clicked(bool)', 
             self.onSaveJson)
+        self.refineVolumeButton.connect('clicked(bool)', self.onRefineVolume)
+        self.registerVolumeButton.connect('clicked(bool)', self.onRegisterVolume)
         self.loadVolumeButton.connect('clicked(bool)', self.onLoadVolume)
         self.loadMaskVolumeButton.connect('clicked(bool)', 
             self.onLoadMaskVolume)
-        
+        self.clearAllVolumeButton.connect('clicked(bool)', 
+            self.onClearAll)
+            
         self.maskIdSelector.connect('currentIndexChanged(int)', 
             self.onMaskIDSelect)
+            
+                #
+        # advanced 
+        #
+        self.advancedCollapsibleButton = ctk.ctkCollapsibleButton()
+        self.advancedCollapsibleButton.text = "Advanced"
+        self.layout.addWidget(self.advancedCollapsibleButton)
+        self.advancedCollapsibleButton.enabled = False
+        
+        self.advancedFormLayout = qt.QFormLayout(self.advancedCollapsibleButton)
+
+        
+
+        # Add vertical spacer
+        self.layout.addStretch(1)
  
     def onLoadJson(self):
         if self.verbose:
@@ -217,7 +271,7 @@ class ParsePathJsonWidget:
         
         self.advancedOptions = []
         for el in jsonUIInfo:
-            print(el)
+            #print(el)
             
             self.idxSlide = ctk.ctkDoubleSpinBox()
             self.advancedFormLayout.addRow("Idx:", self.idxSlide)
@@ -280,6 +334,10 @@ class ParsePathJsonWidget:
             advancedUIEl = [el[0], self.idxSlide, self.rgbPath, self.doRotate, self.doRotate]
             self.advancedOptions.append(advancedUIEl)
 
+
+    def clear_advanced_tab(self):
+        self.advancedFormLayout.removeRow(layout)
+            
     def onSliceIdxChange(self, value, idx):
         if self.verbose:
             print(value,"-",idx)
@@ -320,8 +378,7 @@ class ParsePathJsonWidget:
     def onOpenDialogSaveJson(self):
         if self.verbose:
             print("onSave")
-      
-        print("Current path", self.inputJsonFn.currentPath)
+            print("Current path", self.inputJsonFn.currentPath)
         
         fileDialog = ctk.ctkFileDialog(slicer.util.mainWindow())
         fileDialog.setDirectory(self.inputJsonFn.currentPath)
@@ -342,6 +399,22 @@ class ParsePathJsonWidget:
             self.logic.saveJson(path_out_json)
         except Exception as e:
             print("Coudn't save")
+
+    def onRefineVolume(self):
+        if self.verbose:
+            print("onRefine")
+
+        self.logic.refineVolume(self.inputJsonFn.currentPath,
+            outputVolumeNode = self.outputVolumeSelector.currentNode())
+
+    def onRegisterVolume(self):
+        if self.verbose:
+            print("onRegister")
+
+        self.logic.registerVolume(self.inputJsonFn.currentPath, 
+            self.inputVolumeSelector.currentNode(), 
+            self.inputMaskSelector.currentNode(), 
+            outputVolumeNode = self.outputVolumeSelector.currentNode())
 
     def onLoadVolume(self):
         if self.verbose:
@@ -369,7 +442,26 @@ class ParsePathJsonWidget:
 
         print("Selected Mask", self.idxMask)
 
+    def onLogicEventStart(self):
+        self.currentStatusLabel.text = "Running"
+        #self.progress.setValue(0)
+        self.progress.show()
+        #print("Showing and starting")
 
+    def onLogicEventEnd(self):
+        self.currentStatusLabel.text = "Completed"
+        self.progress.setValue(10)
+        self.progress.hide()
+        
+    def onLogicEventProgress(self, progress):
+        self.currentStatusLabel.text = "Running ({0:6.5f})".format(progress)
+        self.progress.setValue(progress * 1000)
+        
+    def onClearAll(self):
+        self.clear_advanced_tab()
+        qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Not implemented!")
+        
+                
 #
 # parsePath json fusion logic
 #
@@ -378,10 +470,41 @@ class ParsePathJsonLogic():
         self.verbose = True
         self.scriptPath = os.path.dirname(os.path.abspath(__file__))
         self.logic = None
+        
+    def yieldPythonGIL(self, seconds=0):
+        sleep(seconds)
 
+    def cmdStartEvent(self):
+        if hasattr(slicer.modules, 'ParsePathJsonWidget'):
+            widget = slicer.modules.ParsePathJsonWidget
+            widget.onLogicEventStart()
+        self.yieldPythonGIL()
+
+    def cmdProgressEvent(self, progress):
+        if hasattr(slicer.modules, 'ParsePathJsonWidget'):
+            widget = slicer.modules.ParsePathJsonWidget
+            widget.onLogicEventProgress(progress)
+        self.yieldPythonGIL()
+
+    def cmdEndEvent(self):
+        if hasattr(slicer.modules, 'ParsePathJsonWidget'):
+            widget = slicer.modules.ParsePathJsonWidget
+            widget.onLogicEventEnd()
+        self.yieldPythonGIL()
+        
     def loadRgbVolume(self, 
         json_path,
         outputVolumeNode = None):
+        
+        modules = slicer.modules
+        if hasattr(modules, 'ParsePathJsonWidget'):
+            widgetPresent = True
+        else:
+            widgetPresent = False
+
+        if widgetPresent:
+            self.cmdStartEvent()
+            slicer.app.processEvents()
 
         if not self.logic:
             import sys
@@ -390,13 +513,20 @@ class ParsePathJsonLogic():
             import ParsePathJsonUtils as ppju
             self.logic = ppju.ParsePathJsonUtils()
             self.logic.setPath(json_path)
-            self.logic.initComponents()
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
 
         if not str(self.logic.path)==str(json_path):
             self.logic.setPath(json_path)
-            self.logic.initComponents()
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
 
         if outputVolumeNode:
+            slicer.app.processEvents()    
             import sitkUtils
             outputVolume = self.logic.pathologyVolume.loadRgbVolume()
             sitkUtils.PushVolumeToSlicer(outputVolume, 
@@ -406,13 +536,30 @@ class ParsePathJsonLogic():
             selectionNode = slicer.app.applicationLogic().GetSelectionNode()
             selectionNode.SetReferenceActiveVolumeID(outputVolumeNode.GetID())
             slicer.app.applicationLogic().PropagateVolumeSelection(0)
+            
 
 
-   
-    def loadMask(self, 
+        else:
+            qt.QMessageBox.critical(slicer.util.mainWindow(), "No Output","Output Volume was not set!")
+
+        if widgetPresent:
+            self.cmdEndEvent()         
+
+            
+    def refineVolume(self, 
         json_path,
-        idxMask = 0,
-        outputMaskVolumeNode = None):
+        outputVolumeNode = None):
+        
+        modules = slicer.modules
+        if hasattr(modules, 'ParsePathJsonWidget'):
+            widgetPresent = True
+        else:
+            widgetPresent = False
+
+        if widgetPresent:
+            self.cmdStartEvent()
+            slicer.app.processEvents()
+            
         if not self.logic:
             import sys
             sys.path.append(os.path.join(self.scriptPath,"Resources","Utils"))
@@ -420,14 +567,133 @@ class ParsePathJsonLogic():
             import ParsePathJsonUtils as ppju
             self.logic = ppju.ParsePathJsonUtils()
             self.logic.setPath(json_path)
-            self.logic.initComponents()
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
 
         if not str(self.logic.path)==str(json_path):
             self.logic.setPath(json_path)
-            self.logic.initComponents()            
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
+
+        if outputVolumeNode:
+            slicer.app.processEvents()
+            import sitkUtils
+            self.logic.pathologyVolume.registerSlices(False)
+            outputVolume = self.logic.pathologyVolume.loadRgbVolume()
+            sitkUtils.PushVolumeToSlicer(outputVolume, 
+                targetNode=outputVolumeNode)
+        
+
+            selectionNode = slicer.app.applicationLogic().GetSelectionNode()
+            selectionNode.SetReferenceActiveVolumeID(outputVolumeNode.GetID())
+            slicer.app.applicationLogic().PropagateVolumeSelection(0)
+        else:
+            qt.QMessageBox.critical(slicer.util.mainWindow(), "No Output","Output Volume was not set!")
+
+        if widgetPresent:
+            self.cmdEndEvent()       
+
+            
+    def registerVolume(self, 
+        json_path,
+        fixedNode,
+        fixedMaskNode,
+        outputVolumeNode = None):
+        
+        modules = slicer.modules        
+        if hasattr(modules, 'ParsePathJsonWidget'):
+            widgetPresent = True
+        else:
+            widgetPresent = False
+
+        if widgetPresent:
+            self.cmdStartEvent()
+            slicer.app.processEvents()
+            
+        if not self.logic:
+            import sys
+            sys.path.append(os.path.join(self.scriptPath,"Resources","Utils"))
+
+            import ParsePathJsonUtils as ppju
+            self.logic = ppju.ParsePathJsonUtils()
+            self.logic.setPath(json_path)
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
+
+        if not str(self.logic.path)==str(json_path):
+            self.logic.setPath(json_path)
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
+
+        if outputVolumeNode:
+            import sitkUtils
+            slicer.app.processEvents()
+            
+            self.logic.pathologyVolume.imagingContraint = sitkUtils.PullVolumeFromSlicer(fixedNode)
+            self.logic.pathologyVolume.imagingContraintMask = sitkUtils.PullVolumeFromSlicer(fixedMaskNode)
+            self.logic.pathologyVolume.registerSlices(True)
+            
+            outputVolume = self.logic.pathologyVolume.loadRgbVolume()
+            sitkUtils.PushVolumeToSlicer(outputVolume, 
+                targetNode=outputVolumeNode)
+        
+
+            selectionNode = slicer.app.applicationLogic().GetSelectionNode()
+            selectionNode.SetReferenceActiveVolumeID(outputVolumeNode.GetID())
+            slicer.app.applicationLogic().PropagateVolumeSelection(0)            
+        else:
+            qt.QMessageBox.critical(slicer.util.mainWindow(), "No Output","Output Volume was not set!")
+
+        if widgetPresent:
+            self.cmdEndEvent()  
+
+
+           
+    def loadMask(self, 
+        json_path,
+        idxMask = 0,
+        outputMaskVolumeNode = None):
+        
+        modules = slicer.modules
+        if hasattr(modules, 'ParsePathJsonWidget'):
+            widgetPresent = True
+        else:
+            widgetPresent = False
+
+        if widgetPresent:
+            self.cmdStartEvent()
+            slicer.app.processEvents()
+            
+        if not self.logic:
+            import sys
+            sys.path.append(os.path.join(self.scriptPath,"Resources","Utils"))
+
+            import ParsePathJsonUtils as ppju
+            self.logic = ppju.ParsePathJsonUtils()
+            self.logic.setPath(json_path)
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
+
+        if not str(self.logic.path)==str(json_path):
+            self.logic.setPath(json_path)
+            success = self.logic.initComponents()
+            if not success:
+                qt.QMessageBox.critical(slicer.util.mainWindow(), "Error","Failure to load json. Check path!")
+                return
 
 
         if idxMask>=0 and outputMaskVolumeNode:
+            slicer.app.processEvents()
             import sitkUtils
             outputVolume = self.logic.pathologyVolume.loadMask( idxMask )
             sitkUtils.PushVolumeToSlicer(outputVolume, 
@@ -437,7 +703,12 @@ class ParsePathJsonLogic():
             selectionNode = slicer.app.applicationLogic().GetSelectionNode()
             selectionNode.SetReferenceActiveLabelVolumeID(outputMaskVolumeNode.GetID())
             slicer.app.applicationLogic().PropagateVolumeSelection(0)
+        else:
+            qt.QMessageBox.critical(slicer.util.mainWindow(), "No Output Mask","Output Mask was not set!")
 
+        if widgetPresent:
+            self.cmdEndEvent()     
+            
     def getJsonInfo4UI(self, json_path):
         if self.verbose:
            print("Reading json", json_path)
@@ -516,6 +787,10 @@ class ParsePathJsonLogic():
         self.logic.pathologyVolume.saveJson(path)
         
 
+    def deleteData(self):
+        print("Deleting Volume from logic")
+        self.logic.PathologyVolume.deleteData()
+        self.logic = None
             
     def test(self):
         print("Starting the test")
